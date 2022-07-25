ecd6e784bc02ce0d85e62165713806d21112667a
con_schy <connorschultze1@gmail.com>
2022-07-24 23:45:27 -0700

    lessgo

---
+++ background.js
@@ -0,0 +1,58 @@
+chrome.runtime.onInstalled.addListener(details => {
+  chrome.storage.local.clear();
+});
+chrome.tabs.onUpdated.addListener((tabId, tab)=> {
+    if (tab.status == "complete") {
+        chrome.tabs.sendMessage(tabId, {start: true});
+    }
+});
+
+chrome.runtime.onMessage.addListener((request, sender) => {
+    //console.log(request)
+    chrome.storage.local.set({["tab"+sender.tab.id]:request}); 
+    chrome.storage.local.get("tab"+sender.tab.id).then(dat => {
+        
+        if (request.totalTot <= 5){
+            var colorString = "#32a852";
+            }
+        else if (request.totalTot <= 14){
+                var colorString = "#8ECA2E";
+        }
+        else if (request.totalTot <= 24){
+                var colorString = "#f4e03a";
+        }
+        else if (request.totalTot <= 35){
+                var colorString = "#f18931";
+        }
+        else {
+                var colorString = "#ff0d21";
+        }
+        
+        chrome.action.setBadgeBackgroundColor({ 
+            color: colorString,
+            tabId: sender.tab.id
+        });
+        chrome.action.setBadgeText({
+            text: request.totalString,
+            tabId: sender.tab.id
+        });
+    });
+});
+
+// Clear Cache
+chrome.tabs.onRemoved.addListener(tabId => {
+    chrome.storage.local.get('tab'+tabId).then(data => {
+        delete data['tab' + tabId];
+    });
+});
+
+
+chrome.runtime.onInstalled.addListener(function (object) {
+    let externalUrl = "https://ko-fi.com/globemallow#paypalModal";
+
+    if (object.reason === chrome.runtime.OnInstalledReason.INSTALL) {
+        chrome.tabs.create({ url: externalUrl }, function (tab) {
+            //console.log("New tab launched with http://yoursite.com/");
+        });
+    }
+});
---
+++ popup.js
@@ -0,0 +1,57 @@
+chrome.tabs.query({
+    active: true,currentWindow: true
+}).then(tabs => {
+    var tab = tabs[0];
+    chrome.storage.local.get("tab"+tab.id).then(data => {
+        try{
+ var x = data["tab"+tab.id];
+            
+            
+document.getElementById("scoreHTML").innerHTML = x.totalString;
+    
+var scriptNum = x.totalTot;     
+
+switch (scriptNum >= 0){
+        
+case scriptNum >= 0:
+var ctx = document.getElementById('myChart').getContext('2d');
+var yValues = [x.totalAnal, x.totalAd, x.totalChat];
+var barColors = [
+  "#f18931",
+  "#ebdc3d",
+  "#158DCF",
+
+];
+    break;
+
+}
+            
+/*var analLab = x.totalAnal.toString() + " Analytics"; 
+var adLab = x.totalAd.toString() + " Ads";*/
+            
+new Chart(ctx, {
+  type: "doughnut",
+  data: {
+    labels: [' Analytics', ' Ads', ' Chats'],
+    //labels: [analLab, adLab],
+    datasets: [{
+      backgroundColor: barColors,
+      data: yValues
+    }]
+  },
+  options:{
+    plugins:{
+     tooltips:{
+         yAlign: 'top'
+     }
+  }
+  }
+});
+    
+        }
+        catch(e) {
+            //alert("Page is almost loaded")
+        }
+        });
+    
+});
